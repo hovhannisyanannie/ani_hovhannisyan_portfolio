ef33394ec4a21c3f6b6fca2f3cbedd5b65d13ccd
hovhannisyanannie <eny.hovhannisyan@mail.ru>
2023-08-17 18:27:04 +0400

    changed About.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashLink as Link } from 'react-router-hash-link';
-import { Typewriter } from "react-simple-typewriter"
+import { Typewriter } from "react-simple-typewriter";
 import './home.css';
 
 
