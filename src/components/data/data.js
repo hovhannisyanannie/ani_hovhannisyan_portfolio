54602d4b1c64da07c763398cbc9ea7ab2875c8fc
hovhannisyanannie <eny.hovhannisyan@mail.ru>
2023-11-03 14:27:58 +0400

    changed images

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,7 +1,7 @@
 export const portfolio = [
   {
     id: 1,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322828231_947179446646538_9051590649107459274_n.png?_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=E08-6t8CVsQAX-mbVPi&_nc_ht=scontent.fevn7-1.fna&oh=03_AdS99lK6BuL59SMpJyP2ZpFz440tEP1fSo67si7ZO2Qbzw&oe=65439D8F",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322828231_947179446646538_9051590649107459274_n.png?_nc_cat=101&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=SNJwZRbNvDEAX9UgGKK&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTg2a0ZxrAfmtmQXjqrkyfaHMGbEuUO9ViZ8WqIbL7BnA&oe=656C43CF",
     category: "React",
     title: "Online BookStore",
     technologies:"This is an online bookshop, where I tried to use the minimal functionality intended for bookshops. The app allows you to add books to the basket, increase/decrease the number of books, or delete the given book. Pagination, search function/Authors page/ have also been added to the app. On the contact page, there is an option to send me an email via Email.js.I created my fake JSON server to have book data. This app is responsive.",
@@ -10,7 +10,7 @@ export const portfolio = [
   },
   {
     id: 2,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/325012093_430662552527493_5839721797762352025_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=w4hN1qpATnQAX_OK43L&_nc_ht=scontent.fevn7-1.fna&oh=03_AdRxX38MdS45QhzbthcRJ892t7ZR8RH4xlqo3YiHp7WFWw&oe=6543B5D5",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/325012093_430662552527493_5839721797762352025_n.png?_nc_cat=100&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=VwNwL1OmPw8AX9BTBEq&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTNl0SERfm1w3g2pa158DRA8bxgK7cnMK7XN1aX_djusg&oe=656C23D5",
     category: "React/Redux",
     title: "Lora online shop",
     technologies:"This is a React-Redux online shop app that allows you to move to a single product page, add products to the cart, increase/ decrease the number of products, remove products. The products can be sorted by category, brand, alphabetical order, ascending and descending order of price. I used react-toastify on the login and registration pages. I have created a fake JSON server for this app as well. The app is responsive.",
@@ -42,7 +42,7 @@ export const portfolio = [
   },
   {
     id: 5,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321527660_534239165325338_2762036322905988236_n.png?_nc_cat=111&ccb=1-7&_nc_sid=ae9488&_nc_ohc=odZ8LkAy23MAX_X5D-L&_nc_ht=scontent.fevn7-1.fna&oh=03_AdT5ULydC1REfEBSmBFtP_79K5TOC7feH2vsK8wwms2wfA&oe=65439B42",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321527660_534239165325338_2762036322905988236_n.png?_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=IHeQMx_nJNIAX_hEBWv&_nc_ht=scontent.fevn7-1.fna&oh=03_AdQN4Wm4u9wKsrLa7OgZJR_dZB4YpSa6N-5dLY1OHvxXKQ&oe=656C4182",
     category:"JavaScript",
     title: "Tic Tac Toe",
     technologies:"This is a game developed using Javascript. I tried to use as simple codes as possible. The game is also responsive.",
