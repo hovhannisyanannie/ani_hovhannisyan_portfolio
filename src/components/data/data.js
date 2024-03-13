47da39f8f29c6abf05640de8ae8c126669868d67
Ani Hovhannisyan <eny.hovhannisyan@mail.ru>
2024-03-13 23:07:06 +0400

    changed images

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,7 +1,7 @@
 export const portfolio = [
   {
     id: 1,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322828231_947179446646538_9051590649107459274_n.png?_nc_cat=101&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=-Y2rtKld7QMAX_kPzmr&_nc_ht=scontent.fevn7-1.fna&oh=03_AdT10TzL0wpGSfRt5K0gTSgbpo3XpM_IQYaDHV0lWhAwSQ&oe=65EF38CF",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322828231_947179446646538_9051590649107459274_n.png?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=6YCr4rGiBEkAX-Zr26D&_nc_ht=scontent.fevn7-1.fna&oh=03_AdRezz02WFb-OL_tUUYkUa1UVcdE7dURBsNx8qcL-LR7fA&oe=661968CF",
     category: "React",
     title: "Online BookStore",
     technologies:"This is an online bookshop, where I tried to use the minimal functionality intended for bookshops. The app allows you to add books to the basket, increase/decrease the number of books, or delete the given book. Pagination, search function/Authors page/ have also been added to the app. On the contact page, there is an option to send me an email via Email.js.I created my fake JSON server to have book data. This app is responsive.",
@@ -10,7 +10,7 @@ export const portfolio = [
   },
   {
     id: 2,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/325012093_430662552527493_5839721797762352025_n.png?_nc_cat=100&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=B1odGBrtBgAAX9SK8I4&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTVVOT1R0OcwHfcuwsgriWH2D5ybDlrzjsM5iWCOjn5QA&oe=65EF5115",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/325012093_430662552527493_5839721797762352025_n.png?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=m7jttb8SyoEAX8Y_Z5P&_nc_ht=scontent.fevn7-1.fna&oh=03_AdS94D_X7m2GxY2bazJQnEyu4JJ1wzHSI7rBKEknzi4MNg&oe=66198115",
     category: "React/Redux",
     title: "Lora online shop",
     technologies:"This is a React-Redux online shop app that allows you to move to a single product page, add products to the cart, increase/ decrease the number of products, remove products. The products can be sorted by category, brand, alphabetical order, ascending and descending order of price. I used react-toastify on the login and registration pages. I have created a fake JSON server for this app as well. The app is responsive.",
@@ -21,7 +21,7 @@ export const portfolio = [
 
   {
     id: 3,
-    image:"https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/387561867_347016774493227_2889177851285271171_n.png?_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=j2B3LqNoLq8AX831TWm&_nc_ht=scontent.fevn7-1.fna&oh=03_AdQ-bTI8-XznZtxAlbbN0iflzZ0YaxbzgPa5iuxHv4-Yew&oe=65EF55B9",
+    image:"https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/387561867_347016774493227_2889177851285271171_n.png?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fyhV3p9fR7EAX-5u-KG&_nc_ht=scontent.fevn7-1.fna&oh=03_AdSwmozkdc_Ozoqlvn9zXChAW4-eLdQeNnzrbxZRMQx0AA&oe=66194D79",
     category: "React/TypeScript",
     title: "Star Wars API",
     technologies:"This project is made using React and TypeScript. The Star Wars API was used to load the data. The project has pagination, search bar and a separate page for each figure.",
@@ -33,7 +33,7 @@ export const portfolio = [
 
   {
     id: 4,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321241850_567402068262629_8699491391814818158_n.png?_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=mH6iiJz5LBkAX8H8B0V&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTAoBHS-_5yTG5SyJD1yiajLk1lR2EWu3K7LQghyzyYoQ&oe=65EF428C",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321241850_567402068262629_8699491391814818158_n.png?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=GbSTvu-QWxkAX-zppJ6&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTnii7yevwJbsqroDw3c012DoM5yjtgYrpHEkHIEja-Yg&oe=6619728C",
     category: "React",
     title: "React Quiz App",
     technologies:"This is a game developed using React.js. The game is also responsive.",
@@ -42,7 +42,7 @@ export const portfolio = [
   },
   {
     id: 5,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321527660_534239165325338_2762036322905988236_n.png?_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=XTQLTOpKRSIAX86_ebg&_nc_ht=scontent.fevn7-1.fna&oh=03_AdSeh9QjdC2COqSnnMqYnsOawfJUiSTgF2k9MmzjvtDBbA&oe=65EF3682",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/321527660_534239165325338_2762036322905988236_n.png?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qwRMpBd1YaYAX_en8ul&_nc_ht=scontent.fevn7-1.fna&oh=03_AdQ2jTZzx3bJnupumvnycQu8m5bEDkLCeG-_4xtTd1Pkhw&oe=66196682",
     category:"JavaScript",
     title: "Tic Tac Toe",
     technologies:"This is a game developed using Javascript. I tried to use as simple codes as possible. The game is also responsive.",
@@ -51,7 +51,7 @@ export const portfolio = [
   },
   {
     id: 6,
-    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322527496_913726669797339_7235995833285498593_n.png?_nc_cat=110&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=i1KAkJDH9QkAX-VoOAh&_nc_ht=scontent.fevn7-1.fna&oh=03_AdTUZeXVyR0bvz-J25iM1PaXQR9Xhd6V7vns7nBFcwpL3g&oe=65EF5D86",
+    image: "https://scontent.fevn7-1.fna.fbcdn.net/v/t1.15752-9/322527496_913726669797339_7235995833285498593_n.png?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=U9GurbCAjsEAX8AHt-E&_nc_ht=scontent.fevn7-1.fna&oh=03_AdQl94EF_4OEwTE0GqrVi9EerSc4cFH6ZYr7yI8u5urIMg&oe=66195546",
     category: "HTML/CSS",
     title: "Solar Sysyem Website",
     technologies:"The website is made only with HTML/CSS. CSS features have been used as much as possible. There are a number of animations that can be viewed from the solar system drop-down menu. There are also read more/read less, back to top buttons made using only HTML/CSS. The website is responsive․",
